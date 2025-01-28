c8359be0d463fd8baee244675b68b9c91f63bcf4
Ketan-Chaudhary <ketanchaudharypoonia@gmail.com>
2025-01-28 22:51:33 +0530

    ssl

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,3 @@
-// import React from "react";
-// import Form from "./components/Form";
-// import UploadedData from "./components/UploadedData";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Upload Data</h1>
-//       <Form />
-//       <h2>Uploaded Data</h2>
-//       <UploadedData />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// src/App.jsx
 import { Link, Route, Routes } from "react-router-dom";
 import Form from "./components/Form";
 import UploadedData from "./components/UploadedData";
--- client/src/components/UploadedData.jsx
+++ client/src/components/UploadedData.jsx
@@ -7,7 +7,7 @@ const UploadedData = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://13.235.48.219:5000/upload/data");
+        const response = await fetch("https://13.235.48.219:5000/upload/data");
         const result = await response.json();
 
         if (result.message === "No data available.") {
